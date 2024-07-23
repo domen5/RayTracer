d85c8e17de441dc882387183020e34f6a9d23ecd
Andrea <andrea.domeniconi@outlook.com>
2024-07-23 16:42:23 +0000

    Create utility class vec3 to represent colors and geometric points

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <vec3.h>
+
 int main() {
 
     // Image
---
+++ src/vec3.h
@@ -0,0 +1,57 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+class vec3
+{
+public:
+    double e[3];
+
+    vec3() : e{0, 0, 0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    double x() const { return e[0]; }
+    double y() const { return e[1]; }
+    double z() const { return e[2]; }
+
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double &operator[](int i) { return e[i]; }
+
+    vec3 &operator+=(const vec3 &v)
+    {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+
+        return *this;
+    }
+
+    vec3 &operator*=(double c)
+    {
+        e[0] *= c;
+        e[1] *= c;
+        e[2] *= c;
+
+        return *this;
+    }
+
+    vec3 &operator/=(double c)
+    {
+        return *this *= 1 / c;
+    }
+
+    double length_squared() const
+    {
+        return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+    }
+
+    double length() const
+    {
+        return std::sqrt(length_squared());
+    }
+};
+
+#endif
